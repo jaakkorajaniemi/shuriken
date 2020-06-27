c9a3acd99c9f55b9007d50c75fe20fc702974442
Jaakko <jaakko@rajaniemi.fi>
2020-06-27 11:14:55 +0900

    header 10

--- shuriken/apps/core/forms.py
+++ shuriken/apps/core/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Your Name'}))
+    subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Question about work'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Write your message body here'}), required=True)
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'placeholder':'your.own@email.com'}))
